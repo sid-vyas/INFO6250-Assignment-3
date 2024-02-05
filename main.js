2424f3cf30282694ff04782b30788ea2b806aa5d
Siddharth Vyas <vyas.si@northeastern.edu>
2024-02-05 16:44:28 -0500

    Make session management system using a simple survey client

---
+++ main.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const crypto = require('crypto');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+const app = express();
+const sessions = {};
+const sessionDuration = 60 * 60 * 1000; // 1 hour
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', (req, res) => {
+  const sessionId = req.cookies.sessionId || crypto.randomBytes(16).toString('hex');
+  res.cookie('sessionId', sessionId, { httpOnly: true, path: '/' });
+
+  let sessionData = sessions[sessionId] || {};
+  sessionData.lastAccessed = Date.now();
+
+  res.sendFile(path.join(__dirname, 'views', 'client.html'));
+
+  sessions[sessionId] = sessionData;
+});
+
+app.post('/survey', (req, res) => {
+  const sessionId = req.cookies.sessionId;
+  let sessionData = sessions[sessionId] || {};
+
+  const { name, age } = req.body;
+  sessionData = { name, age, lastAccessed: Date.now() };
+
+  console.log(sessionData);
+
+  res.sendFile(path.join(__dirname, 'views', 'surveySubmitted.html'));
+
+  sessions[sessionId] = sessionData;
+});
+
+app.get('/getData', (req, res) => {
+  const sessionId = req.cookies.sessionId;
+  const sessionData = sessions[sessionId] || {};
+
+  if (sessionData.lastAccessed && Date.now() - sessionData.lastAccessed > sessionDuration) {
+    res.send('Session has expired.');
+  } else {
+    res.send(`Session ID: ${sessionId}<br>Session Data: ${JSON.stringify(sessionData)}`);
+  }
+});
+
+app.get('/surveySubmitted', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'surveySubmitted.html'));
+});
+
+app.get('/results', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'results.html'));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
